16d760bd861ec77f3a9ffdeda856b36194cc6f10
Ben McNicholl <git@benmcnicholl.com>
2025-05-21 11:01:13 +1000

    chore: basic main.rs implementation for LS
    Signed-off-by: Ben McNicholl <git@benmcnicholl.com>

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use buildkite_ls::server::Backend;
+use tower_lsp::{LspService, Server};
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+
+    let stdin = tokio::io::stdin();
+    let stdout = tokio::io::stdout();
+
+    let (service, socket) = LspService::new(|client| Backend::new(client));
+    Server::new(stdin, stdout, socket).serve(service).await;
+}
